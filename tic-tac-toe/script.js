c13a20a71cb64847f875c2ee7deaefa87a0ddeae
Jason Castillo <j.moses.castillo@gmail.com>
2020-06-23 10:53:09 -0700

    Implement tic-tac-toe game

---
+++ tic-tac-toe/script.js
@@ -0,0 +1,107 @@
+// 5 parts
+
+// 1. Initial states
+// anonymous function
+window.onload = function() {
+  var num;
+  var box;
+  var ctx;
+  var turn = 1;
+  var filled;
+  var symbol;
+  var winner;
+  var gameOver = false;
+
+  filled = [false, false, false, false, false, false, false, false, false];
+  symbol = ['', '', '', '', '', '', '', '', ''];
+  winner = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
+  // 2. NewGame - event + function
+  // Create a new game click event
+  var n = document.getElementById("new");
+  n.addEventListener("click", newGame);
+
+  // newGame function
+  function newGame() {
+    document.location.reload();
+  }
+
+  // 3. Canvas click + retrieving the box's number
+  // canvas click event
+  document.getElementById("tic").addEventListener("click", function(e)
+  {
+    boxClick(e.target.id);
+  });
+
+  function boxClick(numId) {
+    box = document.getElementById(numId);
+    ctx = box.getContext("2d");
+
+    switch(numId) {
+      case 'canvas1' : num = 1
+                    break;
+      case "canvas2": num = 2
+                    break;
+      case "canvas3": num = 3;
+                    break;
+      case "canvas4": num = 4;
+                    break;
+      case "canvas5": num = 5;
+                    break;
+      case "canvas6": num = 6;
+                    break;
+      case "canvas7": num = 7;
+                    break;
+      case "canvas8": num = 8;
+                    break;
+      case "canvas9": num = 9;
+                    break;
+    }
+
+    // Drawing the shapes on the canvases
+    if(filled[num-1] == false) {
+      if(gameOver == false) {
+        if(turn % 2 == 1) {
+          // Draw X
+          ctx.beginPath();
+          ctx.moveTo(15,15);
+          ctx.lineTo(85,85);
+          ctx.moveTo(85,15);
+          ctx.lineTo(15,85);
+          ctx.strokeStyle = "dodgerblue";
+          ctx.stroke();
+          ctx.closePath();
+          symbol[num-1] = 'X';
+        } else {
+          // Draw O
+          ctx.beginPath();
+          ctx.arc(50, 50, 35, 0, 2*Math.PI, false);
+          ctx.strokeStyle = "dodgerblue";
+          ctx.stroke();
+          ctx.closePath();
+          symbol[num-1] = 'O';
+        }
+        turn++;
+        filled[num-1] = true;
+
+        // 5. Winner check
+        var s = symbol[num-1];
+        for (var j = 0; j < winner.length; j++) {
+          // winner[0] = [0,1,2]
+          // symbol[0], symbol[1], symbol[2]
+          // and operator - condition 1 && condition 2
+          if ((symbol[winner[j][0]] == s) && (symbol[winner[j][1]] == s) && (symbol[winner[j][2]] == s)) {
+            // Found the winner 's'
+            document.getElementById("result").innerText = "Player '" + s + "' won!";
+            gameOver = true;
+          }
+        }
+        // draw condition
+        // turn > 0 or gameOver == false
+        if(turn > 9 && gameOver == false) {
+          document.getElementById("result").innerText = "GAME OVER! IT WAS A DRAW!";
+        }
+      }
+    }
+  }
+}
